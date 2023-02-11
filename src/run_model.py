75cefb4720244b9df2b93c8af87065210adfc7bf
enigmurl <43832426+enigmurl@users.noreply.github.com>
2023-02-11 09:28:33 -0800

    Remove ran sampling calls for testing

--- src/run_model.py
+++ src/run_model.py
@@ -88,10 +88,10 @@ if __name__ == '__main__':
         torch.cuda.empty_cache()
 
         model.train()
-        prev_error = torch.zeros((64, 8, frames.shape[-2], frames.shape[-1]), device=device)
-        im, prev_error = model(xx, prev_error)
-        ran_sample(model, im, prev_error,
-                   frames[:, 60:62])
+        # prev_error = torch.zeros((64, 8, frames.shape[-2], frames.shape[-1]), device=device)
+        # im = model(xx)
+        # ran_sample(model, im, prev_error,
+        #            frames[:, 60:62])
         print("hash", hash(model))
         emse, = train_epoch(train_loader, base, trans, query, optimizer, hammer, con_list, error_fun)
 
